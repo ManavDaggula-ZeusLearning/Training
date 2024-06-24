c123bb05d650756b6fa692f9b327ca26a2858ad1
ManavDaggula-ZeusLearning <manav.daggula@zeuslearning.com>
2024-06-24 18:32:23 +0530

    Added 3 day tasks

---
+++ Day 3 Task/dashboard.js
@@ -0,0 +1,22 @@
+var menuPanel = document.querySelector("#menu-panel")
+var menuBtn = menuPanel.querySelector("&>span")
+// console.log(menuBtn)
+menuBtn.addEventListener("click", (e)=>{
+    e.target.parentElement.dataset["state"] = e.target.parentElement.dataset["state"]==="open" ? "closed" : "open"
+})
+
+var menuItems = document.querySelectorAll(".menu-items")
+// console.log(menuItems);
+menuItems.forEach(element => {
+    
+    console.log(element)
+    let elementListItems = element.querySelectorAll("li");
+    elementListItems.forEach(x => {
+        // console.log(x)
+        x.addEventListener("click",(e)=>{
+            // elementListItems.forEach(l=>l.removeAttribute("data-current"))
+            element.querySelector("[data-current]").removeAttribute("data-current")
+            x.setAttribute("data-current","")
+        })
+    })
+});
