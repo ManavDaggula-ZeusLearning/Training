2e8702de77f566b5a940d06b5e56b6325fff91a1
ManavDaggula-ZeusLearning <manav.daggula@zeuslearning.com>
2024-06-27 15:25:22 +0530

    side menu somewhat working

--- Day 4 Task/Dashboard/dashboard.js
+++ Day 4 Task/Dashboard/dashboard.js
@@ -1,16 +1,30 @@
 var menuPanel = document.querySelector("#menu-panel")
 var menuBtn = document.querySelector(".menu-btn")
-console.log(menuBtn)
+// console.log(menuBtn)
 menuBtn.addEventListener("click", (e)=>{
-    // e.target.parentElement.dataset["state"] = e.target.parentElement.dataset["state"]==="open" ? "closed" : "open"
-    menuPanel.dataset["state"] = menuPanel.dataset["state"]==="open" ? "closed" : "open"
+    menuPanel.dataset["state"] = menuPanel.dataset["state"]!=="clicked" ? "clicked" : "closed"
 })
+
+menuBtn.addEventListener("mouseover", (e)=>{
+    if(menuPanel.dataset["state"]==="closed"){
+        menuPanel.dataset["state"] = "open";
+    }
+})
+
+menuBtn.addEventListener("mouseleave", (e)=>{
+    if(menuPanel.dataset["state"]==="open"){
+        menuPanel.dataset["state"] = "closed";
+    }
+})
+
+
 
 // var menuItems = document.querySelectorAll(".menu-items")
 // console.log(menuItems);
+
 // menuItems.forEach(element => {
     
-//     console.log(element)
+//     // console.log(element)
 //     let elementListItems = element.querySelectorAll("li");
 //     elementListItems.forEach(x => {
 //         // console.log(x)
@@ -21,3 +35,49 @@ menuBtn.addEventListener("click", (e)=>{
 //         })
 //     })
 // });
+
+let menuItems = document.querySelector(".menu-items")
+let liLists = menuItems.querySelectorAll("&>li")
+// console.log(liLists)
+liLists.forEach(l => {
+    l.addEventListener("click", ()=>{
+        liLists.forEach(x => x.removeAttribute("data-current"))
+        l.setAttribute("data-current","");
+    })
+})
+
+
+let smallMenuContainer = document.querySelector("nav .menu .menu-items")
+console.log(smallMenuContainer)
+let subMenus = smallMenuContainer.querySelectorAll("ul")
+subMenus.forEach(sm=>{
+    console.log(sm, sm.scrollHeight)
+})
+
+let smallLis = smallMenuContainer.querySelectorAll("&>li")
+// console.log(smallLis)
+smallLis.forEach(li => {
+    li.addEventListener("click", (e) => {
+        if(e.target.dataset["current"]){
+            // e.target.removeAttribute("data-current");
+            // e.target.querySelector("ul")?.style.height = "0px";
+            return;
+        }
+        // console.log(e.target)
+        smallLis.forEach(x => {
+            x.removeAttribute("data-current");
+            // console.log("removed datacurrent")
+            let y = x.querySelector("ul")
+            if(y){
+                y.style.height = "0px";
+                // console.log("ul collapsed")
+            }
+        })
+        let child = e.target.querySelector("ul");
+        if(child){
+            // console.log(e.target.dataset["current"]);
+            child.style.height = (child.scrollHeight * 2) + "px";
+        }
+        e.target.setAttribute("data-current","true");
+    })
+})
