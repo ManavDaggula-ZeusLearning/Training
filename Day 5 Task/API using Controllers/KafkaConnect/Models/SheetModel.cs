32669b095553ec1eb6c7ec89aff6d0ba21da8a2b
ManavDaggula-ZeusLearning <manav.daggula@zeuslearning.com>
2024-09-04 18:56:24 +0530

    added kafka

---
+++ Day 5 Task/API using Controllers/KafkaConnect/Models/SheetModel.cs
@@ -0,0 +1,13 @@
+// using MongoDB.Bson;
+// using MongoDB.Bson.Serialization.Attributes;
+
+// namespace KafkaConnect.Models
+// {
+//     public class Sheet{
+//         [BsonId]
+//         [BsonRepresentation(BsonType.ObjectId)]
+//         public string? Id { get; set; }
+//         public required string fileId{get;set;}
+//         public double percentage{get;set;} = 0;
+//     }
+// }
